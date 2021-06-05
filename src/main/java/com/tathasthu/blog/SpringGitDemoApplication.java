0025e5e04b6e54071b7e11f6c253ed532429e687
Nilesh <nileshtayade666@gmail.com>
2021-06-06 00:41:35 +0530

    Spring Initial Commit

---
+++ src/main/java/com/tathasthu/blog/SpringGitDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tathasthu.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGitDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tathasthu/blog/SpringGitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tathasthu.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
